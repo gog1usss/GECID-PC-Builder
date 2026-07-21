304973719a013bfc8234fb044a9b1e328ab09e35
gog1usss <greengewot@gmail.com>
2026-07-21 15:41:29 +0300

    --chnaged select to base (id added)

--- components/coolers.py
+++ components/coolers.py
@@ -11,16 +11,20 @@ async def get_all_routers(cpu_id: int = None, cur = Depends(get_db_cursor)):
 
     if cpu_id:
         query = '''
-        select DISTINCT co.brand_cooler, co.name_cooler, co.max_tdp, co.price, co.image_url, cs.cooler_socket, c.name_cpu, c.tdp from cooler co 
+        select DISTINCT co.id, co.brand_cooler, co.name_cooler, co.max_tdp, co.price, co.image_url, cs.cooler_socket, c.name_cpu, c.tdp 
+        from cooler co 
         join cooler_socket cs ON (co.id = cs.cooler_id)
         join cpu c ON (cs.cooler_socket = c.socket_cpu)
         where c.id = %s AND co.max_tdp >= c.tdp;
         '''
-        await cur.execute(query,(cpu_id))
+        await cur.execute(query, (cpu_id,))
 
     else:
-        await cur.execute('select co.brand_cooler, co.name_cooler, co.max_tdp, co.price, co.image_url, cs.cooler_socket from cooler co '
-                    'Join cooler_socket cs ON (co.id = cs.cooler_id);')
+        await cur.execute('''
+        select co.id, co.brand_cooler, co.name_cooler, co.max_tdp, co.price, co.image_url, cs.cooler_socket 
+        from cooler co 
+        Join cooler_socket cs ON (co.id = cs.cooler_id);
+        ''')
 
     coolers = await cur.fetchall()
 
@@ -29,4 +33,3 @@ async def get_all_routers(cpu_id: int = None, cur = Depends(get_db_cursor)):
         'count': len(coolers),
         'data': coolers
     }
-
--- components/ram.py
+++ components/ram.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
 from Database.db import get_db_cursor
 
-
 router = APIRouter(
     prefix='/api/v1/ram',
     tags = ['RAM']
@@ -12,14 +11,15 @@ async def get_all_ram(mother_id: int = None, cur = Depends(get_db_cursor)):
 
     if mother_id:
         query = '''
-        select r.brand_ram, r.name_ram, r.ram_type, r.capacity_ram, r.frequency_mhz, r.price_ram, r.image_url, m.brand_mother, 
+        select r.id, r.brand_ram, r.name_ram, r.ram_type, r.capacity_ram, r.frequency_mhz, r.price_ram, r.image_url, m.brand_mother, 
         m.name_mother, m.socket_mother, m.ram_type from ram r 
         Join motherboard m ON (r.ram_type = m.ram_type)
         WHERE m.id = %s
         '''
-        await cur.execute(query,(mother_id))
+        await cur.execute(query, (mother_id,))
+        ram_1 = await cur.fetchall() 
     else:
-        await cur.execute('select r.brand_ram, r.name_ram, r.ram_type, r.capacity_ram, r.frequency_mhz, r.price_ram, image_url from ram r')
+        await cur.execute('select r.id, r.brand_ram, r.name_ram, r.ram_type, r.capacity_ram, r.frequency_mhz, r.price_ram, image_url from ram r')
         ram_1 = await cur.fetchall()
 
     return {
@@ -27,4 +27,3 @@ async def get_all_ram(mother_id: int = None, cur = Depends(get_db_cursor)):
         'count': len(ram_1),
         'data': ram_1
     }
-
